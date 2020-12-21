bfd8f55f16139bc48ae2d8a0b86c726272f0ecb4
LYDong <384910059@qq.com>
2020-12-21 15:30:51 +0800

    initial

---
+++ Interview questions/sort.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+void sort(int* array, int length);
+int main()
+{
+    int array[]={45,56,76,234,1,34,23,2,3}; //数字任//意给出
+    printf("sizeof(array): %d sizeof(array[0]: %d\n", (int)sizeof(array), (int)sizeof(array[0]));
+    int length = sizeof(array)/ sizeof(array[0]);
+    sort(array, length);
+    return 0;
+}
+
+void sort(int* array, int length)
+{
+    printf("Array length: %d\n", length);
+
+    for(int i = 0; i < length; i++ ){
+        int min = i;
+        for(int j = i + 1; j < length; j++){
+            if(array[min] < array[j]){
+                min = j;
+            }
+        }
+        if(min != i){
+            int mid = array[i];
+            array[i] = array[min];
+            array[min] = mid;
+        }
+    }
+    for(int i = 0; i < length; i++){
+        printf("array[%d]: %d\n", i, array[i]);
+    }
+
+}
+ 
